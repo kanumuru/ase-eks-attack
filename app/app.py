79a22cf1a9057c963f8d9ea9d072a16874c238ee
Rajesh Reddy <kanumururajeshreddy@gmail.com>
2022-02-24 14:51:10 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, request
 import os
-import redis
+
 import subprocess
 
 app = Flask(__name__)
 data =[]
-
-
-# redisClient = redis.StrictRedis(host=os.getenv('redisip'),port=6379,db=0,decode_responses=True)
-redisClient = redis.StrictRedis(host='localhost',port=6379,db=0,decode_responses=True)
 
 @app.route("/", methods=["GET","POST"])
 def get_email():
@@ -18,19 +14,9 @@ def get_email():
 
     if request.method == "POST":
         if request.form.get("email"):
-            query = "email*"
-            keys = redisClient.keys(query)
-            for j in keys:
-                if redisClient.get(j) == request.form.get("email"):
-                    temp =  request.form.get("email") + " has already part of  APPsecengineer subscription"
-                    return render_template("email.html",data=temp)
             
-            if len(keys)<=0:
-                keyid=1
-            else:
-                keyid=len(keys)+1
-            count="email"+str(keyid)
-            redisClient.set(count, request.form.get("email"))
+            
+
         email = request.form.get("email")
         print(email)
         temp =  request.form.get("email") + " has succesfully subscribed to APPsecengineer"
