20f660cfed5b8fe3155c9d7ea064739007ab158d
Kanumuru Rajesh <kanumururajesh@Kanumurus-MacBook-Air.local>
2022-04-06 12:18:59 +0530

    updated the app for SSRF from RCE

--- app/app.py
+++ app/app.py
@@ -8,22 +8,25 @@ data =[]
 
 @app.route("/", methods=["GET","POST"])
 def get_email():
+    temp = ""
     if request.method == "GET":
 
-        return render_template("email.html")
+        return render_template("email.html",data=temp)
 
     if request.method == "POST":
         if request.form.get("email"):
             email = request.form.get("email")
             print(email)
             temp =  request.form.get("email") + " has succesfully subscribed to APPsecengineer"
+            process = subprocess.Popen(['curl', email,'-H',header], stdout=subprocess.PIPE)
+            print(process)
         else:
             print("Empty string")
             temp =  "please enter valid E-mail Address"
-        subprocess.run(["python3", "-c",email])
-
+        # subprocess.run(["python3", "-c",email])
         
-        return render_template("email.html",data=temp)
+        
+        return render_template("email.html",data=process)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=4000)
